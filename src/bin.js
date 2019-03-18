327f699c6adcee82651c1ad4e0d959751f793dcc
c0d01a5 <christopher.dieringer@walmartlabs.com>
2019-03-18 11:30:38 -0700

    feat: add motto flag

--- src/bin.js
+++ src/bin.js
@@ -7,19 +7,21 @@ var cli = meow(
   `
     Usage
       $ cosmos <input>
-
     Options
       --motto, -m  Show the cosmonauts motto
-
     Examples
       $ cosmos listen # listen for cosmonaut travel requests
       $ cosmos --motto
 `,
   {
     flags: {
-      version: {
+      motto: {
         type: 'boolean',
         alias: 'm'
+      },
+      version: {
+        type: 'boolean',
+        alias: 'v'
       }
     }
   }
